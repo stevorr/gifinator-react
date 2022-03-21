f9fe74b19ead66ef32c7cbfe518c6cedc1ee3d43
stevorr <soorr91@gmail.com>
2022-03-21 12:45:07 -0400

    Update Giphy.jsx

--- src/components/Giphy.jsx
+++ src/components/Giphy.jsx
@@ -5,8 +5,8 @@ export default function Giphy() {
     const context = useContext(AppContext)
 
     return (
-        <div className={`max-h-fit max-w-fit px-1`}>
-            <img className="rounded-xl" src={context.data.images.original.url} alt={context.data.title} />
+        <div className={`max-h-fit px-1`}>
+            <img className="shadow rounded-xl" src={context.data.images.original.url} alt={context.data.title} />
         </div>
     )
 }
